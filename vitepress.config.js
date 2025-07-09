b50a44205bf42d93130d1f81dff6b12ab5769812
lxy123hh <1677837750@qq.com>
2025-07-09 14:17:52 +0800

    feat:update config

--- vitepress.config.js
+++ vitepress.config.js
@@ -1,19 +1,24 @@
 module.exports = {
   title: '我的学习笔记',
-  description: '记录我学习过程中遇到的技术点',
+  description: '记录我学习过程中的各种知识点',
   themeConfig: {
+    // 导航栏配置
     nav: [
       { text: '首页', link: '/' },
-      { text: '面试题', link: '/test' },
-      { text: 'Vue.js', link: '/vue' },
+      { text: 'Test', link: '/test' },  // 在导航栏中添加 Test 页面链接
     ],
+    // 侧边栏配置
     sidebar: {
-      '/js/': [
+      '/': [
         {
-          text: 'JavaScript 笔记',
-          children: ['/js/notes']
+          text: '首页',
+          link: '/'
+        },
+        {
+          text: 'Test',
+          link: '/test',  // 在侧边栏中添加 Test 页面链接
         }
       ]
     }
   }
-}
+};
